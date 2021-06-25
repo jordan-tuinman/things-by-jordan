b4ef7b869f9e50d16b1c9cf1772b8a75a0a5f194
jordan-tuinman <jojotuijo@gmail.com>
2021-06-25 12:00:51 +1200

    create globalStyles component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { GlobalStyle } from "./styles/globalStyles"
 import Header from "./header"
 
 const Layout = ({ children }) => {
@@ -17,6 +18,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
---
+++ src/components/styles/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components"
+
+// TODO: add green fuz
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
